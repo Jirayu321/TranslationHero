470394ec1dc731a30c63d94734d8a513614041e6
Jirayu321 <jyung169@gmail.com>
2022-11-23 12:50:14 +0700

    update 23 Nov 65 test6

---
+++ client/build/precache-manifest.9206cb0e5fd04bce0151d36197ba8204.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea095c3baf649a84ad8127485fc6a375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140d3170600bf0f7b0f6",
+    "url": "/static/css/2.ce16ea6b.chunk.css"
+  },
+  {
+    "revision": "62db1214320a75d3fd1a",
+    "url": "/static/css/main.25268ee2.chunk.css"
+  },
+  {
+    "revision": "140d3170600bf0f7b0f6",
+    "url": "/static/js/2.a44f7e5e.chunk.js"
+  },
+  {
+    "revision": "37bb643e1f38b92726858b0eb0a2e1a3",
+    "url": "/static/js/2.a44f7e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7044066f4b5773b0f6",
+    "url": "/static/js/3.8947ddf4.chunk.js"
+  },
+  {
+    "revision": "62db1214320a75d3fd1a",
+    "url": "/static/js/main.7f58121e.chunk.js"
+  },
+  {
+    "revision": "ea41651a17ae579c5c50",
+    "url": "/static/js/runtime-main.e4b25d65.js"
+  },
+  {
+    "revision": "9e1eeb43834aa4e26364e56c5df21e4b",
+    "url": "/static/media/App_store.9e1eeb43.png"
+  },
+  {
+    "revision": "db0b179b29ef1f884c9df4df0f350272",
+    "url": "/static/media/Avatar7.db0b179b.png"
+  },
+  {
+    "revision": "306b4c191bd671b5b7d45a5fcf88cae2",
+    "url": "/static/media/CV.306b4c19.jpeg"
+  },
+  {
+    "revision": "62716352d4c5024d3b0e44aeabebfad8",
+    "url": "/static/media/CompanyCertificate.62716352.jpeg"
+  },
+  {
+    "revision": "62fc0616bb496bd6c2f73f288d5547d0",
+    "url": "/static/media/DivorceCertificate.62fc0616.png"
+  },
+  {
+    "revision": "a97c379ced863457c4fb24b08c00f331",
+    "url": "/static/media/DivorceRegistration.a97c379c.png"
+  },
+  {
+    "revision": "74d39d9726035a53faa9590f85bacd55",
+    "url": "/static/media/Documents-amico.74d39d97.png"
+  },
+  {
+    "revision": "7c636fd134877d12387cb7adfdc15e58",
+    "url": "/static/media/Education.7c636fd1.jpeg"
+  },
+  {
+    "revision": "efd7e77e1da8c7ccf810c1d3b9b252d4",
+    "url": "/static/media/IDcard.efd7e77e.jpeg"
+  },
+  {
+    "revision": "073839c9e49fe3596c8ed0438b572b7b",
+    "url": "/static/media/MarriageCertificate.073839c9.png"
+  },
+  {
+    "revision": "95aab564f8584ac919c1ba3b7b878492",
+    "url": "/static/media/Online_document.95aab564.png"
+  },
+  {
+    "revision": "647f1abba382345abbdb30a49aad7459",
+    "url": "/static/media/Selecting_team.647f1abb.png"
+  },
+  {
+    "revision": "f972faf31158b3ab69a409492893ef2c",
+    "url": "/static/media/Sign_up.f972faf3.png"
+  },
+  {
+    "revision": "4ee41dcba493531bd9c11c9d7a4fe1e8",
+    "url": "/static/media/Translator-bro.4ee41dcb.png"
+  },
+  {
+    "revision": "111db15537af25ebf37472573be5f6ed",
+    "url": "/static/media/login.111db155.png"
+  },
+  {
+    "revision": "10dc96508abdc15f7f7856f727ef76eb",
+    "url": "/static/media/logo.10dc9650.svg"
+  },
+  {
+    "revision": "18c8f4aa1d1afc1ed7973c820300c3de",
+    "url": "/static/media/watermark.18c8f4aa.png"
+  }
+]);
