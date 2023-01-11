553b5e92785d75c33285d3499e5e4466ee2d8902
Jirayu321 <jyung169@gmail.com>
2023-01-11 09:50:01 +0700

    11 jan 66 vtest_9

--- server/utils/generateAuthToken.js
+++ server/utils/generateAuthToken.js
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 const generateAuthToken = (user) => {
   dotenv.config();
 
-  const jwtSecretKey = supersecretkey474485;
+  const jwtSecretKey = "supersecretkey474485";
   console.log("jwtSecretKey:", jwtSecretKey);
   const token = jwt.sign(
     { _id: user._id, name: user.name, email: user.email, type: user.type },
