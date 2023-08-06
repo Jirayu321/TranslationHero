a654eaef8bb08ab5abeff6b72db7c9c169eb6f1d
Jirayu Yung <50855666+Jirayu321@users.noreply.github.com>
2023-08-06 19:00:41 +0700

    6 Aug 66 v8

--- client/src/Components/Dashboard/Dashboard_freelance.js
+++ client/src/Components/Dashboard/Dashboard_freelance.js
@@ -329,7 +329,7 @@ export default function Dashboard_freelance() {
               </div>
 
               <button
-                className="box_salary"
+                className="Dashboard_box_salary"
                 onMouseEnter={() => setHovering(true)}
                 onMouseLeave={() => setHovering(false)}
                 onClick={() => setOpen(true)}
