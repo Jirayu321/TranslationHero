944274624451059af7dc71a125ce695f1351305d
Jirayu Yung <50855666+Jirayu321@users.noreply.github.com>
2023-12-11 08:15:35 +0700

    11 Dec 66 v_api

--- client/src/slices/api.js
+++ client/src/slices/api.js
@@ -1,6 +1,6 @@
-// export const url = "https://sserver-42uf.onrender.com/api";
+export const url = "https://sserver-42uf.onrender.com/api";
 
-export const url = "http://localhost:3001/api";
+// export const url = "http://localhost:3001/api";
 
 export const setHeaders = () => {
   const token = localStorage.getItem("token");
